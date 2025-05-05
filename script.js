b13b48badf94b7d19a37e890d5d8361afb249df3
vinnaver8 <francisvinnaver8@gmail.com>
2025-05-05 05:52:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const svg = document.getElementById('indox-svg');
+svg = document.getElementById('indox-svg');
 const steps = [
   document.getElementById('step1'),
   document.getElementById('step2'),
@@ -84,3 +84,33 @@ function typeAIText() {
 
   typeChar();
 }
+
+const slider = document.querySelector('.slider');
+let isDown = false, startX, scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+  slider.style.cursor = 'grabbing';
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+  slider.style.cursor = 'grab';
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+  slider.style.cursor = 'grab';
+});
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1.5; // scroll-fast multiplier
+  slider.scrollLeft = scrollLeft - walk;
+});
+
+
